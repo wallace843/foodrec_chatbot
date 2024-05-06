c54a14f9a471d8d10ed9ee30f108e38d7bbee50e
wallace843 <wallacesantana843@gmail.com>
2024-05-06 06:09:21 -0300

    modificação do template

--- foodrec_chatbot/settings.py
+++ foodrec_chatbot/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['recbot.azurewebsites.net']
+ALLOWED_HOSTS = ['127.0.0.1','recbot.azurewebsites.net']
 
 
 # Application definition
