e278324ab729c83f865ce43f6183dea2d722db0c
wallace843 <wallacesantana843@gmail.com>
2024-06-12 12:22:45 -0300

    teste2

--- recbot/views.py
+++ recbot/views.py
@@ -2,11 +2,11 @@ from django.template import loader
 from django.http import HttpResponse, JsonResponse
 from recbot.scripts.rag import RAG
 
-
+rag = RAG()
 
 def recbotResponse(request):
     text = request.GET.get('text')
-    response = 'oi'
+    response = rag.output(text)
     dict_response = {"response" : response}
     return JsonResponse(dict_response)
 
